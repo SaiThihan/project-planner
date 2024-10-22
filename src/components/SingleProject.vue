a0215f6369ca5cb733f881bef24d208ae233b748
Sai Thihan <saithihan668@gmail.com>
2024-10-22 21:27:04 +0630

    delete request to db.json

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,32 +1,40 @@
 <template>
-   <div class="project">
-    <div class="actions">
-        <h3 @click="showDetails = !showDetails">{{ project.title }}</h3>
-        <div class="icons">
-            <span class="material-icons">edit</span>
-            <span class="material-icons">delete</span>
-            <span class="material-icons">done</span>
+    <div class="project">
+        <div class="actions">
+            <h3 @click="showDetails = !showDetails">{{ project.title }}</h3>
+            <div class="icons">
+                <span class="material-icons">edit</span>
+                <span @click="deleteProject" class="material-icons">delete</span>
+                <span class="material-icons">done</span>
+            </div>
+        </div>
+        <div v-if="showDetails" class="details">
+            <p>{{ project.details }}</p>
         </div>
     </div>
-    <div v-if="showDetails" class="details">
-        <p>{{ project.details }}</p>
-    </div>
-   </div>
 </template>
 
 <script>
 export default {
-    props:['project'],
-    data(){
+    props: ['project'],
+    data() {
         return {
-            showDetails: false
+            showDetails: false,
+            uri: 'http://localhost:3000/projects/' + this.project.id
+        }
+    },
+    methods: {
+        deleteProject() {
+            fetch(this.uri, {
+                method: 'DELETE',
+            }).then(() => this.$emit('delete', this.project.id)).catch(err => console.log(err))
         }
     }
 }
 </script>
 
 <style>
-.project{
+.project {
     margin: 20px auto;
     background: white;
     padding: 10px 20px;
@@ -35,24 +43,24 @@ export default {
     border-left: 4px solid #e90074;
 }
 
-h3{
+h3 {
     cursor: pointer;
 }
 
-.actions{
+.actions {
     display: flex;
     justify-content: space-between;
     align-items: center;
 }
 
-.material-icons{
+.material-icons {
     font-size: 24px;
     margin-left: 10px;
     color: #bbb;
     cursor: pointer;
 }
 
-.material-icons:hover{
+.material-icons:hover {
     color: #777;
 }
 </style>
