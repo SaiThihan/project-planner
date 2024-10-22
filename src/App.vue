e71e757e3ce7337b25448506b04b704882b07787
Sai Thihan <saithihan668@gmail.com>
2024-10-22 22:03:44 +0630

    complete check,add new pj,navbar form added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
+  <Navbar />
   <router-view />
 </template>
+<script>
+import Navbar from './components/Navbar.vue'
+export default {
 
+  components: {
+    Navbar
+  }
+}
+</script>
 <style>
-
 body {
   background: #f2f2f2;
 }
